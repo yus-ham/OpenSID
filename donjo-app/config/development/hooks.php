cebcf382dbad2778c796c46f781ecb539d74b015
Yusuphm <supalpuket@gmail.com>
2018-11-07 20:28:13 +0700

    untuk membantu pengembangan

--- donjo-app/config/development/hooks.php
+++ donjo-app/config/development/hooks.php
@@ -8,16 +8,19 @@ if (is_file($hooks_php)) {
 
 $hook['pre_controller'][] = function()
 {
-	session_start();
-	log_message('debug', 'Session: '. session_id() ."\n". print_r($_SESSION,8));
 };
 
 $hook['display_override'] = array(function()
 {
 	$CI = CI_Controller::get_instance();
-	$path = 'vendor/ci-debug';
+	$path = FCPATH.'vendor/ci-debug';
 	$CI->load->add_package_path($path);
 	require_once $path .'/hooks/debug_toolbar.php';
 	(new debug_toolbar)->render();
 	$CI->hooks->call_hook('post_system');
 });
+
+$hook['post_controller_constructor'][] = function()
+{
+	log_message('debug', 'Call uac::run()');
+};
