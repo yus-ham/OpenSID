f13988c121e7126807080c897a673478c8cef820
Yusuphm <supalpuket@gmail.com>
2018-11-21 21:00:14 +0700

    Fix: CSRF token not sent

--- assets/js/script.js
+++ assets/js/script.js
@@ -418,25 +418,28 @@ $(function(){
 {
 	var csrf_param = $('meta[name=csrf-param]').attr('content')
 
-	function getCsrfToken()
-	{
+	function getCsrfToken() {
 		return document.cookie.match(new RegExp(csrf_param +'=(\\w+)'))[1]
 	}
 
-	$(document).on('submit', 'form', function()
-	{
-		if (this.method.toLowerCase() !== 'get')
-		{
-			this[csrf_param] || $(this).append($('<input type=hidden name='+ csrf_param +'>'))
-			this[csrf_param].value = getCsrfToken()
+	function addCsrfField(form) {
+		if (form.method.toUpperCase() !== 'GET') {
+			form[csrf_param] || $(form).append($('<input type=hidden name='+ csrf_param +'>'))
+			form[csrf_param].value = getCsrfToken()
 		}
+	}
+
+	$('form').each(function(i, form) {
+		addCsrfField(form)
+	})
+
+	$(document).on('submit', 'form', function()	{
+		addCsrfField(this)
 	})
 
 	// automatically send CSRF token for all AJAX requests
-	$.ajaxPrefilter(function (options, originalOptions, xhr)
-	{
-		if (!options.crossDomain && options.type !== 'GET')
-		{
+	$.ajaxPrefilter(function (options, originalOptions, xhr) {
+		if (!options.crossDomain && options.type !== 'GET') {
 			options.data = (options.data||'') + '&'+ csrf_param +'='+ getCsrfToken()
 		}
 	})
