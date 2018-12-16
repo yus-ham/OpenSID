faba7e003da7bde25b9320e26d162e02f651db2f
Yusuphm <supalpuket@gmail.com>
2018-12-16 20:13:25 +0700

    Bisa menonaktifkan auth pada mode development

--- donjo-app/config/development/hooks.php
+++ donjo-app/config/development/hooks.php
@@ -4,9 +4,20 @@ is_file(__DIR__ .'/../production/hooks.php') && require __DIR__ .'/../production
 
 $hook['pre_controller'][] = function()
 {
+		if (config_item('no_auth'))
+		{
+   error_reporting(30711);
+   session_start();
+    $_SESSION['sesi'] = 'a8d4080245664ed2049c1b2ded7cac30';
+				$_SESSION['siteman'] = 1;
+				
+				$_SESSION['user'] = 1;
+				$_SESSION['grup'] = 1;
+				$_SESSION['per_page'] = 10;
+		}
 };
 
-$hook['display_override'] = array(function()
+$hook['display_override...'] = array(function()
 {
 	$CI = CI_Controller::get_instance();
 	$path = FCPATH.'vendor/ci-debug';
